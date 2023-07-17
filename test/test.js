a4751257f0a485bcadf754056061f619343001ea
Anshul Patel <anshul.patel@opstree.com>
2023-07-17 18:22:38 +0530

    Adding reporter mochaawesome

--- test/test.js
+++ test/test.js
@@ -5,3 +5,30 @@ describe('GET /will', function() {
         request(app).get('/will').expect('{ "response": "Hello World" }', done);
     });
 });
+describe('GET /', function() {
+  it('responds with 200 status code', function(done) {
+    request(app).get('/').expect(200, done);
+  });
+
+  it('responds with JSON containing a message', function(done) {
+    request(app).get('/').expect('Content-Type', /json/).expect({ message: 'Welcome to the API' }, done);
+  });
+});
+
+describe('POST /user', function() {
+  it('responds with 201 status code and success message', function(done) {
+    request(app)
+      .post('/user')
+      .send({ name: 'John', email: 'john@example.com' })
+      .expect(201)
+      .expect({ message: 'User created successfully' }, done);
+  });
+
+  it('responds with 400 status code and error message for invalid request', function(done) {
+    request(app)
+      .post('/user')
+      .send({ email: 'john@example.com' })
+      .expect(400)
+      .expect({ error: 'Name is required' }, done);
+  });
+});
